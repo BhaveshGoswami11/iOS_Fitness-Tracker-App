bb5bb3ced7cba3f882ba7805c62fc7d14c57206b
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-11-26 10:44:10 -0600

    Create Goswami_Practice_Exam02Tests.swift

---
+++ Fitness Tracker AppTests/Goswami_Practice_Exam02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Goswami_Practice_Exam02Tests.swift
+//  Goswami_Practice_Exam02Tests
+//
+//  Created by Bhavesh on 10/28/25.
+//
+
+import Testing
+@testable import Goswami_Practice_Exam02
+
+struct Goswami_Practice_Exam02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
